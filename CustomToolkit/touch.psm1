d56bf4651d1c1b85c47cf6f367baa08f33052c5a
Tyler <streuta@cdmsmith.com>
2020-08-25 12:44:36 -0500

    Added touch module to create new files (aka linux way).

---
+++ CustomToolkit/touch.psm1
@@ -0,0 +1,17 @@
+
+function touch
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        New-Item $file
+    }
+}
